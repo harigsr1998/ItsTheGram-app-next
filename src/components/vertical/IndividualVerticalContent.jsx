3a6cfe90c82262d03eac1a7314d4e3f77a20ab22
Harigovind SR <125468329+harigsr1998@users.noreply.github.com>
2024-11-26 16:11:18 +0530

    Chapter 3 day 1.2 - Change post images to load from static

--- src/components/vertical/IndividualVerticalContent.jsx
+++ src/components/vertical/IndividualVerticalContent.jsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import VerticalContentControls from "./VerticalContentControls";
 
 const IndividualVerticalContent = ({ page, dataParams }) => {
-  const imageRandomizer = Math.floor(Math.random() * 1000);
+  const imageRandomizer = Math.floor(Math.random() * 24);
 
   return (
     <div className="relative h-fit w-full shrink-0">
       <Image
-        src={`https://picsum.photos/608/932?random=${imageRandomizer}`}
+        src={`/random_post_pictures/${imageRandomizer}.jpg`}
         width={608}
         height={932}
         className={`h-full w-full snap-center object-cover`}
